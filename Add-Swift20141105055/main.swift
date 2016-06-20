22f5d78f6ba49c45c59290032f7002bbbe86ed52
卢佳明 <571836353@qq.com>
2016-06-20 10:37:37 +0800

    Initial Commit

---
+++ Add-Swift20141105055/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Add-Swift20141105055
+//
+//  Created by 20141105055ljm on 16/6/20.
+//  Copyright © 2016年 20141105055ljm. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
